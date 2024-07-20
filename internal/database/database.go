67a76795b8a4251d1415646b37709d814a0e4216
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-07-20 12:49:59 -0400

    connect database

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,15 +2,26 @@ package database
 
 import (
 	"log"
+	"os"
 
-	"github.com/joho/godotenv"
+	"github.com/rmarmolejo90/candles_api/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func ConnectDB() {
+	config.LoadEnv()
+	dsn := os.Getenv("DB_DSN")
 
-	err := godotenv.Load()
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error connecting to database: %v", err)
 	}
+	log.Printf("\n\n*** Connected To The Database ***\n\n")
 
+	// Need to add all models for migration
+	err = DB.AutoMigrate()
+	if err != nil {
+		log.Print("Error Migrating the database: " + err.Error())
+	}
 }
