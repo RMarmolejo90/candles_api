30c899cffcb070c9e2d6c285eb715f9beb8f9631
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-07-27 10:35:24 -0400

    add user repository

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -3,5 +3,54 @@ package repository
 import (
 	"github.com/rmarmolejo90/candles_api/internal/database"
 	"github.com/rmarmolejo90/candles_api/internal/models"
+	"gorm.io/gorm"
 )
 
+type UserRepository interface {
+	CreateUser(user models.User) (models.User, error)
+	GetUserByID(id uint) (models.User, error)
+	GetUserByEmail(email string) (models.User, error)
+	GetUserByUsername(username string) (models.User, error)
+	UpdateUser(user models.User) (models.User, error)
+	DeleteUser(id uint) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository() UserRepository {
+	return &userRepository{db: database.DB}
+}
+
+func (r *userRepository) CreateUser(user models.User) (models.User, error) {
+	err := r.db.Create(&user).Error
+	return user, err
+}
+
+func (r *userRepository) GetUserByID(id uint) (models.User, error) {
+	var user models.User
+	err := r.db.Preload("Addresses").Preload("Cart").Preload("Orders").Preload("Reviews").Preload("Roles").First(&user, id).Error
+	return user, err
+}
+
+func (r *userRepository) GetUserByEmail(email string) (models.User, error) {
+	var user models.User
+	err := r.db.Where("email = ?", email).Preload("Addresses").Preload("Cart").Preload("Orders").Preload("Reviews").Preload("Roles").First(&user).Error
+	return user, err
+}
+
+func (r *userRepository) GetUserByUsername(username string) (models.User, error) {
+	var user models.User
+	err := r.db.Where("username = ?", username).Preload("Addresses").Preload("Cart").Preload("Orders").Preload("Reviews").Preload("Roles").First(&user).Error
+	return user, err
+}
+
+func (r *userRepository) UpdateUser(user models.User) (models.User, error) {
+	err := r.db.Save(&user).Error
+	return user, err
+}
+
+func (r *userRepository) DeleteUser(id uint) error {
+	return r.db.Delete(&models.User{}, id).Error
+}
