6d933eb11cc576d1608c8ce855bca40a887d60a3
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:51:51 -0400

    add category controllers

--- internal/controllers/category.go
+++ internal/controllers/category.go
@@ -1,1 +1,85 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type CategoryController struct {
+	categoryService services.CategoryService
+}
+
+func NewCategoryController(categoryService services.CategoryService) *CategoryController {
+	return &CategoryController{categoryService: categoryService}
+}
+
+func (cc *CategoryController) CreateCategory(c *gin.Context) {
+	var category models.Category
+	if err := c.ShouldBindJSON(&category); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdCategory, err := cc.categoryService.CreateCategory(category)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdCategory)
+}
+
+func (cc *CategoryController) GetCategoryByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid category ID"})
+		return
+	}
+	category, err := cc.categoryService.GetCategoryByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, category)
+}
+
+func (cc *CategoryController) UpdateCategory(c *gin.Context) {
+	var category models.Category
+	if err := c.ShouldBindJSON(&category); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedCategory, err := cc.categoryService.UpdateCategory(category)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedCategory)
+}
+
+func (cc *CategoryController) DeleteCategory(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid category ID"})
+		return
+	}
+	err = cc.categoryService.DeleteCategory(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (cc *CategoryController) GetAllCategories(c *gin.Context) {
+	categories, err := cc.categoryService.GetAllCategories()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, categories)
+}
