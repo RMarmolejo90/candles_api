2d03af3ebb4cd47437f47826a0587f009beefdc5
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:48:52 -0400

    add order controllers

--- internal/controllers/order.go
+++ internal/controllers/order.go
@@ -1,1 +1,259 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type OrderController struct {
+	orderService        services.OrderService
+	orderItemService    services.OrderItemService
+	orderStatusService  services.OrderStatusService
+	orderHistoryService services.OrderHistoryService
+}
+
+func NewOrderController(orderService services.OrderService, orderItemService services.OrderItemService, orderStatusService services.OrderStatusService, orderHistoryService services.OrderHistoryService) *OrderController {
+	return &OrderController{
+		orderService:        orderService,
+		orderItemService:    orderItemService,
+		orderStatusService:  orderStatusService,
+		orderHistoryService: orderHistoryService,
+	}
+}
+
+func (oc *OrderController) CreateOrder(c *gin.Context) {
+	var order models.Order
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdOrder, err := oc.orderService.CreateOrder(order)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdOrder)
+}
+
+func (oc *OrderController) GetOrderByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order ID"})
+		return
+	}
+	order, err := oc.orderService.GetOrderByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, order)
+}
+
+func (oc *OrderController) UpdateOrder(c *gin.Context) {
+	var order models.Order
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedOrder, err := oc.orderService.UpdateOrder(order)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedOrder)
+}
+
+func (oc *OrderController) DeleteOrder(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order ID"})
+		return
+	}
+	err = oc.orderService.DeleteOrder(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (oc *OrderController) GetOrdersByUserID(c *gin.Context) {
+	userIDParam := c.Param("userID")
+	userID, err := strconv.ParseUint(userIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+	orders, err := oc.orderService.GetOrdersByUserID(uint(userID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orders)
+}
+
+func (oc *OrderController) CreateOrderItem(c *gin.Context) {
+	var orderItem models.OrderItem
+	if err := c.ShouldBindJSON(&orderItem); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdOrderItem, err := oc.orderItemService.CreateOrderItem(orderItem)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdOrderItem)
+}
+
+func (oc *OrderController) GetOrderItemByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order item ID"})
+		return
+	}
+	orderItem, err := oc.orderItemService.GetOrderItemByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orderItem)
+}
+
+func (oc *OrderController) UpdateOrderItem(c *gin.Context) {
+	var orderItem models.OrderItem
+	if err := c.ShouldBindJSON(&orderItem); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedOrderItem, err := oc.orderItemService.UpdateOrderItem(orderItem)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedOrderItem)
+}
+
+func (oc *OrderController) DeleteOrderItem(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order item ID"})
+		return
+	}
+	err = oc.orderItemService.DeleteOrderItem(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (oc *OrderController) CreateOrderStatus(c *gin.Context) {
+	var orderStatus models.OrderStatus
+	if err := c.ShouldBindJSON(&orderStatus); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdOrderStatus, err := oc.orderStatusService.CreateOrderStatus(orderStatus)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdOrderStatus)
+}
+
+func (oc *OrderController) GetOrderStatusByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order status ID"})
+		return
+	}
+	orderStatus, err := oc.orderStatusService.GetOrderStatusByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orderStatus)
+}
+
+func (oc *OrderController) UpdateOrderStatus(c *gin.Context) {
+	var orderStatus models.OrderStatus
+	if err := c.ShouldBindJSON(&orderStatus); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedOrderStatus, err := oc.orderStatusService.UpdateOrderStatus(orderStatus)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedOrderStatus)
+}
+
+func (oc *OrderController) DeleteOrderStatus(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order status ID"})
+		return
+	}
+	err = oc.orderStatusService.DeleteOrderStatus(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (oc *OrderController) CreateOrderHistory(c *gin.Context) {
+	var orderHistory models.OrderHistory
+	if err := c.ShouldBindJSON(&orderHistory); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdOrderHistory, err := oc.orderHistoryService.CreateOrderHistory(orderHistory)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdOrderHistory)
+}
+
+func (oc *OrderController) GetOrderHistoryByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order history ID"})
+		return
+	}
+	orderHistory, err := oc.orderHistoryService.GetOrderHistoryByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orderHistory)
+}
+
+func (oc *OrderController) GetOrderHistoriesByOrderID(c *gin.Context) {
+	orderIDParam := c.Param("orderID")
+	orderID, err := strconv.ParseUint(orderIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order ID"})
+		return
+	}
+	orderHistories, err := oc.orderHistoryService.GetOrderHistoriesByOrderID(uint(orderID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orderHistories)
+}
--- internal/controllers/review.go
+++ internal/controllers/review.go
@@ -1,1 +1,106 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type ReviewController struct {
+	reviewService services.ReviewService
+}
+
+func NewReviewController(reviewService services.ReviewService) *ReviewController {
+	return &ReviewController{reviewService: reviewService}
+}
+
+func (rc *ReviewController) CreateReview(c *gin.Context) {
+	var review models.Review
+	if err := c.ShouldBindJSON(&review); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdReview, err := rc.reviewService.CreateReview(review)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdReview)
+}
+
+func (rc *ReviewController) GetReviewByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid review ID"})
+		return
+	}
+	review, err := rc.reviewService.GetReviewByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, review)
+}
+
+func (rc *ReviewController) UpdateReview(c *gin.Context) {
+	var review models.Review
+	if err := c.ShouldBindJSON(&review); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedReview, err := rc.reviewService.UpdateReview(review)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedReview)
+}
+
+func (rc *ReviewController) DeleteReview(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid review ID"})
+		return
+	}
+	err = rc.reviewService.DeleteReview(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (rc *ReviewController) GetReviewsByUserID(c *gin.Context) {
+	userIDParam := c.Param("userID")
+	userID, err := strconv.ParseUint(userIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+	reviews, err := rc.reviewService.GetReviewsByUserID(uint(userID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, reviews)
+}
+
+func (rc *ReviewController) GetReviewsByCandleID(c *gin.Context) {
+	candleIDParam := c.Param("candleID")
+	candleID, err := strconv.ParseUint(candleIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid candle ID"})
+		return
+	}
+	reviews, err := rc.reviewService.GetReviewsByCandleID(uint(candleID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, reviews)
+}
