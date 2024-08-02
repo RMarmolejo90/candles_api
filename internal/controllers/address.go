17bbe7441f0ca86debb41241e1a65160edfac2f6
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:58:03 -0400

    add address controller

--- internal/controllers/address.go
+++ internal/controllers/address.go
@@ -1,1 +1,97 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type AddressController struct {
+	addressService services.AddressService
+}
+
+func NewAddressController(addressService services.AddressService) *AddressController {
+	return &AddressController{addressService: addressService}
+}
+
+func (ac *AddressController) CreateAddress(c *gin.Context) {
+	var address models.Address
+	if err := c.ShouldBindJSON(&address); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdAddress, err := ac.addressService.CreateAddress(address)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdAddress)
+}
+
+func (ac *AddressController) GetAddressByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid address ID"})
+		return
+	}
+	address, err := ac.addressService.GetAddressByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, address)
+}
+
+func (ac *AddressController) GetAddressesByUserID(c *gin.Context) {
+	userIDParam := c.Param("userID")
+	userID, err := strconv.ParseUint(userIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+	addresses, err := ac.addressService.GetAddressesByUserID(uint(userID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, addresses)
+}
+
+func (ac *AddressController) UpdateAddress(c *gin.Context) {
+	var address models.Address
+	if err := c.ShouldBindJSON(&address); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid address ID"})
+		return
+	}
+	updatedAddress, err := ac.addressService.UpdateAddress(uint(id), address)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedAddress)
+}
+
+func (ac *AddressController) DeleteAddress(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid address ID"})
+		return
+	}
+	err = ac.addressService.DeleteAddress(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
