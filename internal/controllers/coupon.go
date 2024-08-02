f453643d70db06f65d2707c0128cbb7d433e755f
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:50:43 -0400

    add coupon controller

--- internal/controllers/coupon.go
+++ internal/controllers/coupon.go
@@ -1,1 +1,95 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type CouponController struct {
+	couponService services.CouponService
+}
+
+func NewCouponController(couponService services.CouponService) *CouponController {
+	return &CouponController{couponService: couponService}
+}
+
+func (cc *CouponController) CreateCoupon(c *gin.Context) {
+	var coupon models.Coupon
+	if err := c.ShouldBindJSON(&coupon); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdCoupon, err := cc.couponService.CreateCoupon(coupon)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdCoupon)
+}
+
+func (cc *CouponController) GetCouponByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid coupon ID"})
+		return
+	}
+	coupon, err := cc.couponService.GetCouponByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, coupon)
+}
+
+func (cc *CouponController) GetCouponByCode(c *gin.Context) {
+	code := c.Query("code")
+	coupon, err := cc.couponService.GetCouponByCode(code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, coupon)
+}
+
+func (cc *CouponController) UpdateCoupon(c *gin.Context) {
+	var coupon models.Coupon
+	if err := c.ShouldBindJSON(&coupon); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedCoupon, err := cc.couponService.UpdateCoupon(coupon)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedCoupon)
+}
+
+func (cc *CouponController) DeleteCoupon(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid coupon ID"})
+		return
+	}
+	err = cc.couponService.DeleteCoupon(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (cc *CouponController) GetValidCoupons(c *gin.Context) {
+	coupons, err := cc.couponService.GetValidCoupons()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, coupons)
+}
