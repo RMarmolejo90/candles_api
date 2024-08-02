3b48266b5145592ec2d2beb6d2e7148e44f7e42a
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:53:56 -0400

    add cart controllers

--- internal/controllers/cart.go
+++ internal/controllers/cart.go
@@ -1,1 +1,117 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type CartController struct {
+	cartService services.CartService
+}
+
+func NewCartController(cartService services.CartService) *CartController {
+	return &CartController{cartService: cartService}
+}
+
+func (cc *CartController) CreateCart(c *gin.Context) {
+	var cart models.Cart
+	if err := c.ShouldBindJSON(&cart); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdCart, err := cc.cartService.CreateCart(cart)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdCart)
+}
+
+func (cc *CartController) UpdateCart(c *gin.Context) {
+	var cart models.Cart
+	if err := c.ShouldBindJSON(&cart); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedCart, err := cc.cartService.UpdateCart(cart)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedCart)
+}
+
+func (cc *CartController) GetCartByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid cart ID"})
+		return
+	}
+	cart, err := cc.cartService.GetCartByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, cart)
+}
+
+func (cc *CartController) DeleteCart(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid cart ID"})
+		return
+	}
+	err = cc.cartService.DeleteCart(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (cc *CartController) AddItemToCart(c *gin.Context) {
+	cartIDParam := c.Param("cartID")
+	cartID, err := strconv.ParseUint(cartIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid cart ID"})
+		return
+	}
+	var item models.CartItem
+	if err := c.ShouldBindJSON(&item); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedCart, err := cc.cartService.AddItemToCart(uint(cartID), item)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedCart)
+}
+
+func (cc *CartController) RemoveItemFromCart(c *gin.Context) {
+	cartIDParam := c.Param("cartID")
+	cartID, err := strconv.ParseUint(cartIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid cart ID"})
+		return
+	}
+	itemIDParam := c.Param("itemID")
+	itemID, err := strconv.ParseUint(itemIDParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid item ID"})
+		return
+	}
+	updatedCart, err := cc.cartService.RemoveItemFromCart(uint(cartID), uint(itemID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedCart)
+}
