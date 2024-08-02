f549aa719fb83ac72e03aad9f821e15f32a317da
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:20:19 -0400

    add shipping rates controller

--- internal/controllers/shippingRate.go
+++ internal/controllers/shippingRate.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type ShippingRateController struct {
+	shippingRateService services.ShippingRateService
+}
+
+func NewShippingRateController(shippingRateService services.ShippingRateService) *ShippingRateController {
+	return &ShippingRateController{shippingRateService: shippingRateService}
+}
+
+func (sc *ShippingRateController) GetShippingRate(c *gin.Context) {
+	zipCode := c.Param("zipCode")
+	quantityStr := c.Query("quantity")
+
+	quantity, err := strconv.Atoi(quantityStr)
+	if err != nil || quantity <= 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid quantity"})
+		return
+	}
+
+	rate, err := sc.shippingRateService.GetShippingRate(zipCode, quantity)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"zipCode": zipCode, "quantity": quantity, "rate": rate})
+}
