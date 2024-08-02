853d9b32ce722d58d8d2f97b7f5e96a06aa86a84
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:14:23 -0400

    add tax rate services

--- internal/services/taxRate.go
+++ internal/services/taxRate.go
@@ -1,11 +1,7 @@
 package services
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -59,30 +55,5 @@ func (s *taxRateService) GetTaxRate(zipCode string) (float64, error) {
 }
 
 func (s *taxRateService) fetchTaxRate(zipCode string) (float64, error) {
-	// Example of making an HTTP GET request to an external API
-	apiURL := fmt.Sprintf("https://api.example.com/taxrate?zipCode=%s&apiKey=%s", zipCode, s.taxAPIKey)
-	resp, err := http.Get(apiURL)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return 0, errors.New("failed to fetch rate from tax API")
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	var result struct {
-		Rate float64 `json:"rate"`
-	}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return 0, err
-	}
-
-	return result.Rate, nil
+	// call external tax rate api
 }
