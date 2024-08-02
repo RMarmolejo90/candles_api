8904ab88449cc6bad1df87d2cd8fc24bbd557d74
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:18:57 -0400

    add tax rate controller

--- internal/controllers/taxRate.go
+++ internal/controllers/taxRate.go
@@ -1,1 +1,26 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type TaxRateController struct {
+	taxRateService services.TaxRateService
+}
+
+func NewTaxRateController(taxRateService services.TaxRateService) *TaxRateController {
+	return &TaxRateController{taxRateService: taxRateService}
+}
+
+func (tc *TaxRateController) GetTaxRate(c *gin.Context) {
+	zipCode := c.Param("zipCode")
+	rate, err := tc.taxRateService.GetTaxRate(zipCode)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"zipCode": zipCode, "rate": rate})
+}
