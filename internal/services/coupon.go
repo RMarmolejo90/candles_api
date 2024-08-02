1971a7baac9ce361e8ef560441efe7d41e9efe6d
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 12:27:37 -0400

    add coupon services

--- internal/services/coupon.go
+++ internal/services/coupon.go
@@ -1,7 +1,58 @@
 package services
 
 import (
-	"github.com/rmarmolejo90/candles_api/internal/database"
+	"errors"
+	"time"
+
 	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/repository"
 )
 
+type CouponService interface {
+	CreateCoupon(coupon models.Coupon) (models.Coupon, error)
+	GetCouponByID(id uint) (models.Coupon, error)
+	GetCouponByCode(code string) (models.Coupon, error)
+	UpdateCoupon(coupon models.Coupon) (models.Coupon, error)
+	DeleteCoupon(id uint) error
+	GetValidCoupons() ([]models.Coupon, error)
+}
+
+type couponService struct {
+	couponRepo repository.CouponRepository
+}
+
+func NewCouponService(couponRepo repository.CouponRepository) CouponService {
+	return &couponService{couponRepo: couponRepo}
+}
+
+func (s *couponService) CreateCoupon(coupon models.Coupon) (models.Coupon, error) {
+	// Business logic or validation can be added here before saving
+	if coupon.Code == "" || coupon.Discount <= 0 || coupon.ValidUntil.Before(time.Now()) {
+		return models.Coupon{}, errors.New("invalid coupon details")
+	}
+	return s.couponRepo.CreateCoupon(coupon)
+}
+
+func (s *couponService) GetCouponByID(id uint) (models.Coupon, error) {
+	return s.couponRepo.GetCouponByID(id)
+}
+
+func (s *couponService) GetCouponByCode(code string) (models.Coupon, error) {
+	return s.couponRepo.GetCouponByCode(code)
+}
+
+func (s *couponService) UpdateCoupon(coupon models.Coupon) (models.Coupon, error) {
+	// Additional business logic can be added here
+	if coupon.Code == "" || coupon.Discount <= 0 || coupon.ValidUntil.Before(time.Now()) {
+		return models.Coupon{}, errors.New("invalid coupon details")
+	}
+	return s.couponRepo.UpdateCoupon(coupon)
+}
+
+func (s *couponService) DeleteCoupon(id uint) error {
+	return s.couponRepo.DeleteCoupon(id)
+}
+
+func (s *couponService) GetValidCoupons() ([]models.Coupon, error) {
+	return s.couponRepo.GetValidCoupons()
+}
