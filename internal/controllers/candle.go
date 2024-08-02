c95d9c6b11d32a396e7f409be4f778a613d58ee1
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 13:56:24 -0400

    add candles controller

--- internal/controllers/candle.go
+++ internal/controllers/candle.go
@@ -1,1 +1,163 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/services"
+)
+
+type CandleController struct {
+	candleService services.CandleService
+}
+
+func NewCandleController(candleService services.CandleService) *CandleController {
+	return &CandleController{candleService: candleService}
+}
+
+func (cc *CandleController) CreateNewCandle(c *gin.Context) {
+	var candle models.Candle
+	if err := c.ShouldBindJSON(&candle); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdCandle, err := cc.candleService.CreateNewCandle(candle)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdCandle)
+}
+
+func (cc *CandleController) GetCandleDetails(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid candle ID"})
+		return
+	}
+	candle, err := cc.candleService.GetCandleDetails(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, candle)
+}
+
+func (cc *CandleController) UpdateCandleInfo(c *gin.Context) {
+	var candle models.Candle
+	if err := c.ShouldBindJSON(&candle); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedCandle, err := cc.candleService.UpdateCandleInfo(candle)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedCandle)
+}
+
+func (cc *CandleController) RemoveCandle(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid candle ID"})
+		return
+	}
+	err = cc.candleService.RemoveCandle(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (cc *CandleController) AddImageToCandle(c *gin.Context) {
+	var image models.Image
+	if err := c.ShouldBindJSON(&image); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdImage, err := cc.candleService.AddImageToCandle(image)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdImage)
+}
+
+func (cc *CandleController) RemoveImage(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid image ID"})
+		return
+	}
+	err = cc.candleService.RemoveImage(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (cc *CandleController) CreateStock(c *gin.Context) {
+	var stock models.StockQuantity
+	if err := c.ShouldBindJSON(&stock); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdStock, err := cc.candleService.CreateStock(stock)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, createdStock)
+}
+
+func (cc *CandleController) GetStockDetails(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid stock ID"})
+		return
+	}
+	stock, err := cc.candleService.GetStockDetails(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, stock)
+}
+
+func (cc *CandleController) UpdateStockQuantity(c *gin.Context) {
+	var stock models.StockQuantity
+	if err := c.ShouldBindJSON(&stock); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	updatedStock, err := cc.candleService.UpdateStockQuantity(stock)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, updatedStock)
+}
+
+func (cc *CandleController) RemoveStock(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid stock ID"})
+		return
+	}
+	err = cc.candleService.RemoveStock(uint(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
