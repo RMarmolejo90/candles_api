e0239c169151b988da8046025b24201809ea332a
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-08-02 12:24:07 -0400

    add category services

--- internal/services/cart.go
+++ internal/services/cart.go
@@ -1,7 +1,72 @@
 package services
 
 import (
-	"github.com/rmarmolejo90/candles_api/internal/database"
+	"errors"
+
 	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/repository"
 )
 
+type CartService interface {
+	CreateCart(cart models.Cart) (models.Cart, error)
+	UpdateCart(cart models.Cart) (models.Cart, error)
+	GetCartByID(id uint) (models.Cart, error)
+	DeleteCart(id uint) error
+	AddItemToCart(cartID uint, item models.CartItem) (models.Cart, error)
+	RemoveItemFromCart(cartID, itemID uint) (models.Cart, error)
+}
+
+type cartService struct {
+	cartRepo repository.CartRepository
+}
+
+func NewCartService(cartRepo repository.CartRepository) CartService {
+	return &cartService{cartRepo: cartRepo}
+}
+
+func (s *cartService) CreateCart(cart models.Cart) (models.Cart, error) {
+	// need to check if the user already has a cart before creating a new one
+
+	if cart.UserID == 0 {
+		return models.Cart{}, errors.New("user ID must be provided")
+	}
+	return s.cartRepo.CreateCart(cart)
+}
+
+func (s *cartService) UpdateCart(cart models.Cart) (models.Cart, error) {
+	return s.cartRepo.UpdateCart(cart)
+}
+
+func (s *cartService) GetCartByID(id uint) (models.Cart, error) {
+	return s.cartRepo.GetCartByID(id)
+}
+
+func (s *cartService) DeleteCart(id uint) error {
+	return s.cartRepo.DeleteCart(id)
+}
+
+func (s *cartService) AddItemToCart(cartID uint, item models.CartItem) (models.Cart, error) {
+	cart, err := s.cartRepo.GetCartByID(cartID)
+	if err != nil {
+		return models.Cart{}, err
+	}
+	// add item to cart
+	cart.CartItems = append(cart.CartItems, item)
+	return s.cartRepo.UpdateCart(cart)
+}
+
+func (s *cartService) RemoveItemFromCart(cartID, itemID uint) (models.Cart, error) {
+	cart, err := s.cartRepo.GetCartByID(cartID)
+	if err != nil {
+		return models.Cart{}, err
+	}
+
+	// Remove item logic
+	for i, item := range cart.CartItems {
+		if item.ID == itemID {
+			cart.CartItems = append(cart.CartItems[:i], cart.CartItems[i+1:]...)
+			break
+		}
+	}
+	return s.cartRepo.UpdateCart(cart)
+}
--- internal/services/category.go
+++ internal/services/category.go
@@ -1,7 +1,46 @@
 package services
 
 import (
-	"github.com/rmarmolejo90/candles_api/internal/database"
+	"errors"
+
 	"github.com/rmarmolejo90/candles_api/internal/models"
+	"github.com/rmarmolejo90/candles_api/internal/repository"
 )
 
+type CategoryService interface {
+	CreateCategory(category models.Category) (models.Category, error)
+	GetCategory(id uint) (models.Category, error)
+	UpdateCategory(category models.Category) (models.Category, error)
+	GetAllCategories() ([]models.Category, error)
+}
+
+type categoryService struct {
+	categoryRepo repository.CategoryRepository
+}
+
+func NewCategoryService(categoryRepo repository.CategoryRepository) CategoryService {
+	return &categoryService{categoryRepo: categoryRepo}
+}
+
+func (s *categoryService) CreateCategory(category models.Category) (models.Category, error) {
+	// validation can be added here before saving
+	if category.Name == "" {
+		return models.Category{}, errors.New("category name must be provided")
+	}
+	return s.categoryRepo.CreateCategory(category)
+}
+
+func (s *categoryService) GetCategory(id uint) (models.Category, error) {
+	return s.categoryRepo.GetCategory(id)
+}
+
+func (s *categoryService) UpdateCategory(category models.Category) (models.Category, error) {
+	if category.Name == "" {
+		return models.Category{}, errors.New("category name must be provided")
+	}
+	return s.categoryRepo.UpdateCategory(category)
+}
+
+func (s *categoryService) GetAllCategories() ([]models.Category, error) {
+	return s.categoryRepo.GetAllCategories()
+}
